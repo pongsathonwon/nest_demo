02461ab67eed1adf73c0331e8b51be21c8b19f9d
pongsathonwon <men1234-2008@hotmail.com>
2024-12-24 15:14:53 +0700

    binding prisma exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaExceptionFilter } from './prisma-exception/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  const { httpAdapter } = app.get(HttpAdapterHost)
+  app.useGlobalFilters(new PrismaExceptionFilter(httpAdapter))
   await app.listen(process.env.PORT ?? 3000);
+
+
 }
 bootstrap();
