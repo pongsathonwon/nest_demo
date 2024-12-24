7ef7c0999c2021e5ec39d5e4c567b69da9f83418
pongsathonwon <men1234-2008@hotmail.com>
2024-12-24 14:40:17 +0700

    body validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,7 +1,12 @@
 import { Prisma } from "@prisma/client";
 import { DecimalJsLike } from "@prisma/client/runtime/library";
+import { IsString, IsNumber, IsNotEmpty } from "class-validator"
 
 export class CreateProductDto implements Prisma.ProductsCreateInput {
+    @IsString()
+    @IsNotEmpty()
     readonly name: string;
+    @IsNumber()
+    @IsNotEmpty()
     readonly price: number | Prisma.Decimal | DecimalJsLike;
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class ProductsService {
@@ -15,15 +16,29 @@ export class ProductsService {
   }
 
   async findOne(id: number) {
-    console.log(id)
-    return await this._prisma.products.findUnique({ where: { id } })
+    const result = await this._prisma.products.findUnique({ where: { id } })
+    if (!result) throw new NotFoundException()
+    return result
   }
 
   async update(id: number, updateProductDto: UpdateProductDto) {
-    return await this._prisma.products.update({ where: { id }, data: updateProductDto });
+    try {
+      const result = await this._prisma.products.update({ where: { id }, data: updateProductDto });
+      console.log(result)
+      return result
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   async remove(id: number) {
-    return await this._prisma.products.delete({ where: { id } })
+    try {
+      const result = await this._prisma.products.delete({ where: { id } })
+      return result
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === "P2025") throw new NotFoundException()
+      }
+    }
   }
 }
