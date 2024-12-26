1268e15c3a5ac9fec40e1a846a9c762c0b8a77f1
pongsathonwon <men1234-2008@hotmail.com>
2024-12-26 12:13:15 +0700

    validate user

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,7 +1,14 @@
 import { Prisma } from "@prisma/client";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class CreateAuthDto implements Prisma.UsersCreateInput {
+    @IsEmail()
+    @IsNotEmpty()
     readonly email: string;
+    @IsString()
+    @MinLength(8)
     readonly password: string;
+    @IsString()
+    @IsNotEmpty()
     readonly username: string;
 }
