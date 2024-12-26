eb705a064d16cb7c32c2d3c9eb32a4c69368f73b
pongsathonwon <men1234-2008@hotmail.com>
2024-12-26 15:52:05 +0700

    extract jwt from cookies

--- src/auth/auth.strategy.ts
+++ src/auth/auth.strategy.ts
@@ -4,12 +4,13 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { AuthService } from "./auth.service";
 import { Token } from "./auth.type";
 import { ConfigService } from "@nestjs/config";
+import { Request } from "express";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     constructor(private _auth: AuthService, private _config: ConfigService) {
         super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            jwtFromRequest: ExtractJwt.fromExtractors([(req: Request) => req.cookies?.access_token]),
             ignoreExpiration: false,
             secretOrKey: _config.get("SECRET")
         })
@@ -18,7 +19,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     async validate(payload: Token) {
         const user = await this._auth.checkAuth(payload.userId)
         if (!user) throw new UnauthorizedException()
-        console.log(user)
         return user
     }
 }
