67331ea015dc0ce2256c60b74ceae13ba6ec0447
pongsathonwon <men1234-2008@hotmail.com>
2024-12-26 10:44:29 +0700

    register passport and jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [PassportModule, JwtModule.register({ secret: "mysrc", signOptions: { expiresIn: "5m" } })],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
